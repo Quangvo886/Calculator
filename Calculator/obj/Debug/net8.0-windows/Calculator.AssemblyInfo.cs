f3f7a679f0bc1d6830bc53cd5d44d15b95c785a0
Quangvo886 <thaithien.404061@gmail.com>
2025-02-10 11:59:26 +0700

    Xu ly nut Cong

--- Calculator/obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f58d34e48bee5b02027ba7c300b179334e7f056")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
